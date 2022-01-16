89e03613c48b0a0e5247a875ae8cab6c1eaa5bbb
Sami Eljabali <sami@eljabali.org>
2022-01-15 19:13:45 -0700

    code clean up

--- app/src/main/java/co/touchlab/kampkit/android/landingpage/view/screens/WeatherReportView.kt
+++ app/src/main/java/co/touchlab/kampkit/android/landingpage/view/screens/WeatherReportView.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import co.touchlab.kampkit.android.design.Spacing
 import co.touchlab.kampkit.android.design.styles.TextStyles
 import co.touchlab.kampkit.metaweather.repo.WeatherReport
