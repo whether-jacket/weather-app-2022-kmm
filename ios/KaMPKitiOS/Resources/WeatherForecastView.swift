1d5592bf960e6f22b6f0660b8d4ef12040eba9c4
mo <mo@nicolass-MacBook-Pro-2.local>
2022-01-26 19:57:41 -0700

    Added new file for new structure

---
+++ ios/KaMPKitiOS/Resources/WeatherForecastView.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  KaMPKitiOS
+//
+//  Created by mo on 1/26/22.
+//  Copyright © 2022 Touchlab. All rights reserved.
+//
+
+import Foundation
+import shared
+import SwiftUI
+
+struct WeatherView: View {
+    @StateObject private var sharedViewModel: SharedViewModel = ViewModel.getSharedViewModel.asStateObject
+    
+    var body: some View {
+        
+    
+    }
+}
