93cec10464c93bd5607c8550341c0a34beccf91f
Sami Eljabali <sami@eljabali.org>
2022-02-25 21:56:39 -0700

    remove klint

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,24 +23,6 @@ allprojects {
     }
 }
 
-subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        enableExperimentalRules.set(true)
-        verbose.set(true)
-        filter {
-            exclude { it.file.path.contains("build/") }
-        }
-    }
-
-    afterEvaluate {
-        tasks.named("check").configure {
-            dependsOn(tasks.getByName("ktlintCheck"))
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
