4410a3fa16076455a52e905369a540543e49fdce
Ramzi Jabali <ramzi@eljabali.org>
2022-07-18 18:47:42 -0700

    Added swifter swift, removed swift lint

--- ios/KaMPKitiOS/Resources/Colors.swift
+++ ios/KaMPKitiOS/Resources/Colors.swift
@@ -1,18 +1,18 @@
-//import Foundation
-//import UIKit
-//import SwifterSwift
-//
-//enum PresetColors {
-//    static let white = UIColor(hex: 0xFFFFFF)!
-//    static let aqua = UIColor(hex: 0x00FFFF)!
-//    static let black = UIColor(hex: 0x000000)!
-//    static let blue = UIColor(hex: 0x0000FF)!
-//    static let darkBlue = UIColor(hex: 0x00008B)!
-//    static let gainsboro = UIColor(hex: 0xDCDCDC)!
-//    static let yellow = UIColor(hex: 0xFFFF00)!
-//    static let purple200 = UIColor(hex: 0xFFBB86FC)!
-//    static let purple500 = UIColor(hex: 0xFF6200EE)!
-//    static let purple700 = UIColor(hex: 0xFF3700B3)!
-//    static let teal200 = UIColor(hex: 0xFF03DAC5)!
-//    static let dimGray = UIColor(hex: 0xFF696969)!
-//}
+import Foundation
+import UIKit
+import SwifterSwift
+
+enum PresetColors {
+    static let white = UIColor(hex: 0xFFFFFF)!
+    static let aqua = UIColor(hex: 0x00FFFF)!
+    static let black = UIColor(hex: 0x000000)!
+    static let blue = UIColor(hex: 0x0000FF)!
+    static let darkBlue = UIColor(hex: 0x00008B)!
+    static let gainsboro = UIColor(hex: 0xDCDCDC)!
+    static let yellow = UIColor(hex: 0xFFFF00)!
+    static let purple200 = UIColor(hex: 0xFFBB86FC)!
+    static let purple500 = UIColor(hex: 0xFF6200EE)!
+    static let purple700 = UIColor(hex: 0xFF3700B3)!
+    static let teal200 = UIColor(hex: 0xFF03DAC5)!
+    static let dimGray = UIColor(hex: 0xFF696969)!
+}
--- ios/Pods/Target Support Files/Pods-KaMPKitiOS/Pods-KaMPKitiOS-frameworks.sh
+++ ios/Pods/Target Support Files/Pods-KaMPKitiOS/Pods-KaMPKitiOS-frameworks.sh
@@ -161,9 +161,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/SwifterSwift/SwifterSwift.framework"
   install_framework "${PODS_ROOT}/../../shared/build/cocoapods/framework/shared.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/SwifterSwift/SwifterSwift.framework"
   install_framework "${PODS_ROOT}/../../shared/build/cocoapods/framework/shared.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
